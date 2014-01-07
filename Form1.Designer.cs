b8137e5a827f6c74d34ae6fa5acd2eb961b88971
Raymond Lanoux <raymondlanoux@gmail.com>
2014-01-06 18:43:21 -0600

    initial add

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsObjects
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.cboList = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(463, 450);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(148, 45);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(308, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // cboList
+            // 
+            this.cboList.FormattingEnabled = true;
+            this.cboList.Location = new System.Drawing.Point(12, 38);
+            this.cboList.Name = "cboList";
+            this.cboList.Size = new System.Drawing.Size(308, 21);
+            this.cboList.TabIndex = 2;
+            this.cboList.Text = "(Select one.)";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(326, 13);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(133, 19);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add Item";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 498);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(623, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(39, 17);
+            this.lblStatus.Text = "Ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 520);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cboList);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Form1";
+            this.Text = "Because The Teacher Said So";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ComboBox cboList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsObjects
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //Verify that something is in the text box
+            //If there is something in the text box
+            //      Add it to the list of items in the combo box
+            //Otherwise
+            //      Change the background of the textbox to red
+            //      Otherwise tell the user to enter text
+            if (txtInput.Text != String.Empty)
+            {
+                cboList.Items.Add(txtInput.Text);
+                txtInput.Text = String.Empty;
+            }
+            else
+            {
+                txtInput.BackColor = Color.Red;
+                lblStatus.Text = "You must enter a string value in the textbox.";
+                MessageBox.Show("A problem has occurred. Check status bar.",
+                    "Because The Teacher Said So",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                txtInput.BackColor = Color.White;
+                lblStatus.Text = "Ready";
+                txtInput.Focus();
+            }
+        }
+    }
+}
